64698a0446c47c3518afc30dc8c01c809903bea4
odntht <odntht@gmail.com>
2019-03-21 11:25:56 -0300

    Aula 01 a 03 de Javascript, calculando o IMC a partir dos dados em HTML, validação dos dados informados e também alteração de estilo caso algo não seja validado

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,29 +2,54 @@
 var tituloPagina = document.querySelector(".tituloPagina");
 tituloPagina.textContent = "Aparecida Nutrição";
 
-var paciente = document.querySelector("#primeiro"); //busca todos os dados do paciente #primeiro
-var tdPeso = paciente.querySelector(".info-peso").textContent; //busca o peso do paciente
-var tdAltura = paciente.querySelector(".info-altura").textContent; //busca a altura do paciente
+var pacientes = document.querySelectorAll(".paciente"); //busca todos os dados de todos os pacientes
+
+console.log(pacientes);
+
+console.log(pacientes.length);
+
+console.log(100/2.00);
+
+
+
+for(var i = 0; i < pacientes.length; i++) {
+
+	var paciente = pacientes[i];
+
+var tdPeso = paciente.querySelector(".info-peso"); //busca o peso do paciente
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura"); //busca a altura do paciente
+var altura = tdAltura.textContent;
+
 var tdImc = paciente.querySelector(".info-imc");
 
 var pesoValido = true;
 var alturaValida = true
 
-if(tdPeso <= 0 || tdPeso >= 500) {
+if(peso <= 0 || peso >= 500) {
 	pesoValido = false;
+	tdImc.textContent = "Peso inválido!";
+	paciente.classList.add("paciente-invalido");
 }
 
-if (tdAltura <= 0 || tdAltura >= 3) {
+if (altura <= 0 || altura >= 3) {
 	alturaValida = false;
+	tdImc.textContent = "Altura inválida!";
+	paciente.classList.add("paciente-invalido");
+
 }
 
 if (alturaValida && pesoValido) {
 
-var imc = tdPeso/(tdAltura*tdAltura);
-tdImc.textContent = imc;
+var imc = peso/(altura*altura);
+tdImc.textContent = imc.toFixed(2);
 
-} else {
-	alert("Peso e/ou altura invalidos! \nIMC não calculado!" );
-}
+} 
+
+};
+
+
+
 
 
